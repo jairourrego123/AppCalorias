1bcb1a8e29b04c9bae29b2b860f9049776117918
unknown <jairourrego123@gmail.com>
2023-11-06 21:12:27 -0500

    Ajustes iniciales para la navegacion de goback en la vista Add Food

--- src/pages/AddFood/AddFood.tsx
+++ src/pages/AddFood/AddFood.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Text } from 'react-native'
+import Header from '../../components/Header'
 // import { Text } from '@rneui/themed'
 function AddFood() {
   return (
-    <View>
-        <Text>Add Food</Text>
+    <View style={styles.container}>
+      <Header/>
+       
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container:{
+    padding:12,
+    backgroundColor:'#fff',
+    flex:1,
+  },
+
+})
 export default AddFood
