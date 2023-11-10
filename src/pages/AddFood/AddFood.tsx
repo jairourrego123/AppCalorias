16ab6f707622b4ed5f0c66789090eca8e4fe1c14
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-11-10 10:24:46 -0500

    configuracion de pagina agregando todas las funcionalidades respectivas del aplicartivo

--- src/pages/AddFood/AddFood.tsx
+++ src/pages/AddFood/AddFood.tsx
@@ -1,23 +1,134 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Alert, ScrollView, StyleSheet, View } from 'react-native'
 import { Text } from 'react-native'
 import Header from '../../components/Header'
+import { Button, Icon, Input } from '@rneui/themed';
+import AddFoodModal from '../../components/AddFoodModal';
+import useFoodStorage from '../../hooks/useFoodStorage';
+import { Meal } from '../../types';
+import MealItem from '../../components/MealItem';
 // import { Text } from '@rneui/themed'
 function AddFood() {
-  return (
-    <View style={styles.container}>
-      <Header/>
-       
-    </View>
-  )
-}
+    const [visible, setIsvisible] = useState<boolean>(false)
+    const [foods, setFoods] = useState<Meal[]>([])
+    const [search, setSearch] = useState<string>('')
+    const { onGetFoods } = useFoodStorage()
+    const loadFoods = async () => {
+        try {
+            const foodResponse = await onGetFoods();
+            setFoods(foodResponse)
+
+        } catch (error) {
+            console.error(error);
+
+        }
+    }
+
+
+    useEffect(() => {
+        loadFoods().catch(null)
+    },[])
+    const handleModalClose = async (shouldUpdate?: boolean) => {
+        if (shouldUpdate) {
+            Alert.alert('Comida guardada exitosamente')
+            loadFoods();
+        }
+
+
+        setIsvisible(false)
+    }
+
+    const handleSearchPress = async () => {
+        try {
+        
+            const result = await onGetFoods();
+            console.log(result.filter((item: Meal) => item.name.includes(search)));
+            
+            setFoods(result.filter((item: Meal) => item.name.toLowerCase().includes(search.toLowerCase())));
+        } catch (error) {
+                console.error(error);
+                setFoods([]);
+                
+        }
+    }
+    return (
+        <View style={styles.container}>
+            <Header />
+            <View style={styles.addFoodContainer}>
+                <View style={styles.legendContainer}>
+                    <Text style={styles.addFoodLegend}>Add Food</Text>
+                </View>
+                <View style={styles.addFoodBtnContainer}>
+                    <Button
+
+                        icon={<Icon name="add-circle-outline" color="#fff" />}
+                        radius='lg'
+                        color='#4ecb71'
+                        size='sm'
+                        onPress={() => { setIsvisible(true) }}
+                    //   onPress={handleAddCaloriesPress}
+                    />
+                </View>
+            </View>
+            <View style={styles.searchContainer}>
+                <View style={styles.inputSearch}>
+                    <Input placeholder="apples, fries, soda...." value={search} onChangeText={(text: string) => (setSearch(text))} />
+                </View>
+
+                <Button title="Search" color={"#ade8af"} titleStyle={styles.searchBtnTitle} radius={'lg'} onPress={handleSearchPress} />
+
+            </View>
+            <ScrollView style={styles.content}>
+                {foods?.map(meal => (
+                    <MealItem key={`my-meal-itel-${meal.name}`} {...meal} isAbletoAdd />
+                ))}
+            </ScrollView>
+            <AddFoodModal visible={visible} onClose={handleModalClose} />
+        </View>
+    )
+} 0
 
 const styles = StyleSheet.create({
-  container:{
-    padding:12,
-    backgroundColor:'#fff',
-    flex:1,
-  },
+    container: {
+        padding: 12,
+        backgroundColor: '#fff',
+        flex: 1,
+    },
+    addFoodContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 24,
+
+    },
+    legendContainer: {
+        flex: 1,
+        // alignItems:'flex-start'
+    },
+    addFoodBtnContainer: {
+        flex: 1,
+        alignItems: 'flex-end'
+
+    },
+    addFoodLegend: {
+        fontSize: 20,
+        color: '#000'
+    },
+    searchContainer: {
+        flexDirection: 'row',
+
+
+    },
+    inputSearch: {
+        flex: 1,
+        marginLeft: -12
+        // alignItems:'flex-start',
+        // width:'100%'
+    },
+    searchBtnTitle: {
+        color: "#000",
+        fontSize: 14,
+    },
+    content: {},
 
 })
 export default AddFood
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,67 +1,109 @@
 import { Text } from '@rneui/themed'
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { StyleSheet, View } from 'react-native'
 import Header from '../../components/Header'
-import { Button,Icon } from '@rneui/themed';
-import { useNavigation } from '@react-navigation/native';
+import { Button, Icon } from '@rneui/themed';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamsList } from '../../types';
+import { Meal, RootStackParamsList, TodayCaloriesProps } from '../../types';
+import useFoodStorage from '../../hooks/useFoodStorage';
+import TodayCalories  from '../../components/TodayCalories';
+import TodayMeals from '../../components/TodayMeals';
+
+const totalCaloriesPerDay = 2000;
 const Home = () => {
+  const [todayFood, setTodayFood] = useState<Meal[]>([])
+  const [todayStadistic, setTodayStadistic] = useState<TodayCaloriesProps>({consumed:0,total:totalCaloriesPerDay,remaining:0,porcentage:0});
+  const { onGetTodayFoods } = useFoodStorage();
+  const { navigate } = useNavigation<NativeStackNavigationProp<RootStackParamsList, 'Home'>>();
 
-  const {navigate} = useNavigation<NativeStackNavigationProp<RootStackParamsList,'Home'>>();
 
-  const handleAddCaloriesPress = ()=>{
+  const calculateTodayStadistic = (meals: Meal[]) => {
+    try {
+      const caloriesConsumed = meals.reduce((acum, curr) => acum + Number(curr.calories), 0);
+
+      const remainingCalories = totalCaloriesPerDay - caloriesConsumed;
+      const porcentage = (caloriesConsumed / totalCaloriesPerDay) * 100
+
+      setTodayStadistic({ consumed: caloriesConsumed, porcentage, remaining: remainingCalories });
+    } catch (error) {
+      console.error(error);
+
+    }
+  }
+
+  const loadTodayFood = useCallback(async () => {
+    try {
+      const todyFoodResponse = (await onGetTodayFoods()) as Meal[];
+      calculateTodayStadistic(todyFoodResponse);
+      setTodayFood(todyFoodResponse);
+    } catch (error) {
+      console.error(error);
+      setTodayFood([])
+    }
+
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    loadTodayFood().catch(null);
+
+  }, [loadTodayFood]))
+
+  const handleAddCaloriesPress = () => {
 
     navigate('AddFood')
 
   }
 
+
   return (
     <View style={styles.container}>
-        <Header/>
+      <Header />
       <View style={styles.caloriesContainer}>
         <View style={styles.leftContainer}>
-        <Text style={styles.caloriesLegent}>Calories </Text>
+          <Text style={styles.caloriesLegend}>Calories </Text>
         </View>
         <View style={styles.rightContainer}>
-          <Button 
-          
-            icon={<Icon name="add-circle-outline" color="#fff" />} 
-             radius='lg'
+          <Button
+
+            icon={<Icon name="add-circle-outline" color="#fff" />}
+            radius='lg'
             color='#4ecb71'
             onPress={handleAddCaloriesPress}
-            />
+          />
 
-            
+
         </View>
       </View>
+      <TodayCalories {...todayStadistic} />
+      <TodayMeals foods={todayFood} onCompleteAddRemove={loadTodayFood}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    padding:12,
-    backgroundColor:'#fff',
-    flex:1,
+  container: {
+    padding: 12,
+    backgroundColor: '#fff',
+    flex: 1,
   },
- 
-  caloriesContainer:{
-    flexDirection:'row',
-    alignItems:'center',
-    marginVertical:24,
+
+  caloriesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 24,
   },
-  leftContainer:{
-    flex:1,
-    justifyContent:'center'
+  leftContainer: {
+    flex: 1,
+    justifyContent: 'center'
   },
-  rightContainer:{
-    flex:1,
-    alignItems:'flex-end',
-    justifyContent:'center',
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   },
-  caloriesLegent:{
-    fontSize:20,
+  caloriesLegend: {
+    fontSize: 20,
   },
 })
 
