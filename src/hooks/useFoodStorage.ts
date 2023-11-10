3599d051fb6b5c6e3518fe9b3cb9c0b8c9da4ce7
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-11-10 10:23:39 -0500

    hooks con las funcionalidades de obtener, guardar comidas y eliminar comidas del dia

--- src/hooks/useFoodStorage.ts
+++ src/hooks/useFoodStorage.ts
@@ -1,57 +1,107 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { isToday } from 'date-fns';
+
 const MY_FOOD_KEY = '@MyFood:key';
+const MY_TODAY_FOOD_KEY = '@MyTodayFood:key';
 import { Meal } from '../types';
 const useFoodStorage = () => {
-    const handleSaveFood = async ({ calories, name, portion }: Meal) => {
-        
+    const saveInfoToStorage = async(storageKey: string,meal:Meal) =>{
         try {
-            const currentSaveFood = await AsyncStorage.getItem(MY_FOOD_KEY);
-            
+            const currentSaveFood = await AsyncStorage.getItem(storageKey);
+
             if (currentSaveFood !== null) {
                 const currentSaveFoodParsed = JSON.parse(currentSaveFood);
-                
-           
 
-                currentSaveFoodParsed.push({
-                    calories,
-                    name,
-                    portion,
-                });
-              
-                await AsyncStorage.setItem(MY_FOOD_KEY, JSON.stringify(currentSaveFoodParsed))
+
+
+                currentSaveFoodParsed.push(meal);
+
+                await AsyncStorage.setItem(storageKey, JSON.stringify(currentSaveFoodParsed))
                 return Promise.resolve()
             }
-            await AsyncStorage.setItem(MY_FOOD_KEY, JSON.stringify([{ calories, name, portion }]));
+            await AsyncStorage.setItem(storageKey, JSON.stringify([meal]));
             return Promise.resolve();
 
         } catch (error) {
             return Promise.reject(error)
         }
 
+    }
+    const handleSaveFood = async ({ calories, name, portion }: Meal) => {
+
+        try {
+          const result = await saveInfoToStorage(MY_FOOD_KEY,{calories,name,portion})
+          return Promise.resolve();
+        } catch (error) {
+            return Promise.reject(error)
+        }
+
     };
 
-    const handleGetFoods = async() => {
+    const handleGetFoods = async () => {
 
         try {
             const foods = await AsyncStorage.getItem(MY_FOOD_KEY)
             if (foods !== null) {
                 const parseFoods = JSON.parse(foods)
                 return Promise.resolve(parseFoods)
-                
+
             }
         } catch (error) {
             return Promise.reject(error)
         }
 
     }
+
+    const handleSaveTodayFood = async ({ calories, name, portion }: Meal) => {
+
+        try {
+            const result = await saveInfoToStorage(MY_TODAY_FOOD_KEY,{calories,name,portion, date: new Date().toISOString()})
+            return Promise.resolve()
+        } catch (error) {
+            return Promise.reject()
+        }
+
+    }
+
+    const handleGetTodayFood = async ()=>{
+        try {
+            const foods = await AsyncStorage.getItem(MY_TODAY_FOOD_KEY);
+            if (foods !== null) {
+                const parseFoods = JSON.parse(foods) as Meal [];
+                return Promise.resolve(parseFoods.filter(meal => meal.date && isToday(new Date(meal.date))))
+
+            }
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    }
+    
+    const handleRemoveTodayFood = async (index:number) =>{
+        try {
+            const todayFood = await handleGetTodayFood();
+            const filterItem= todayFood?.filter((item:Meal,itemIndex)=>{
+                return itemIndex!== index
+            })
+
+            await AsyncStorage.setItem(MY_TODAY_FOOD_KEY,JSON.stringify(filterItem))
+            return Promise.resolve();
+        } catch (error) {
+            return Promise.reject(error)
+            
+        }
+    }
     return {
         onSaveFood: handleSaveFood,
-        onGetFoods: handleGetFoods
+        onGetFoods: handleGetFoods,
+        onSaveTodayFoods: handleSaveTodayFood,
+        onGetTodayFoods:handleGetTodayFood,
+        onDeleteTodayFood:handleRemoveTodayFood,
     };
 };
 
-// Guardar informacion de comida
-// Metodo para obtener info de comida
+// Guardar informacion de comida del dia de hoy 
+// Metodo para obtener info de comida del dia de hoy 
 //
 
 export default useFoodStorage
